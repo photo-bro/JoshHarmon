13ea8d61fc94e9ed12ae9fbc714ff2d434cdc464
Josh Harmon <josh.harmon@xero.com>
2019-09-18 15:06:48 -0700

    Add 404 page and routing

--- JoshHarmon.Site/ClientApp/src/App.js
+++ JoshHarmon.Site/ClientApp/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import { Layout } from './components/Layout';
 import { Splash } from './components/Splash/Splash';
 import { Projects } from './components/Projects/Projects';
+import { NotFound } from './NotFound';
 
 export default class App extends Component {
-  static displayName = App.name;
+    static displayName = App.name;
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Splash} />
-        <Route exact path='/projects' component={Projects} />
-      </Layout>
-    );
-  }
+    render() {
+        let exactRoute = window.location.pathname;
+        return (
+            <Layout>
+                <Switch>
+                    <Route exact path='/' component={Splash} />
+                    <Route exact path='/projects' component={Projects} />
+                    <Route render={(p) => <NotFound route={exactRoute} />} />
+                </Switch>
+            </Layout>
+        );
+    }
 }
---
+++ JoshHarmon.Site/ClientApp/src/NotFound.js
@@ -0,0 +1,28 @@
+﻿import React, { Component } from 'react';
+
+export class NotFound extends Component {
+    static displayName = NotFound.name;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            statusCode: props.statusCode,
+            route: props.route,
+        };
+    }
+
+    render() {
+        let error = <h2>There is no resource at '<b>{this.state.route}</b>'</h2>;
+
+        return (
+            <div class="page">
+                <div class="notFound">
+                    <h1><b>Page not found...</b></h1>
+                    {error}
+                    <a href="/">Return to home</a>
+                </div>
+            </div>
+        );
+    }
+}
