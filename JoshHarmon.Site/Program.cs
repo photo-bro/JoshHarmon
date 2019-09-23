08283cea1bc37c9acf6e0d55d1ca68d100168d19
Josh Harmon <josh.harmon@xero.com>
2019-09-23 10:53:04 -0700

    Configured serilog for to log warnings to local file

--- JoshHarmon.Site/Program.cs
+++ JoshHarmon.Site/Program.cs
@@ -1,20 +1,25 @@
 using System;
+using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Serilog;
-using Serilog.Events;
 
 namespace JoshHarmon.Site
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
         public static int Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()
-            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-            .Enrich.FromLogContext()
-            .WriteTo.Console()
+            .ReadFrom.Configuration(Configuration)
             .CreateLogger();
 
             try
@@ -23,7 +28,7 @@ namespace JoshHarmon.Site
                 CreateWebHostBuilder(args).Build().Run();
                 return 0;
             }
-            catch(Exception ex )
+            catch (Exception ex)
             {
                 Log.Fatal(ex, "Host terminated unexpectedly");
                 return 1;
@@ -36,14 +41,9 @@ namespace JoshHarmon.Site
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(Configuration)
+                .ConfigureLogging(log => log.AddSerilog(Log.Logger))
                 .UseStartup<Startup>()
                 .UseSerilog();
-        //.ConfigureLogging((hostingContext, logging) =>
-        //{
-        //    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-        //    logging.AddConsole();
-        //    logging.AddDebug();
-        //    logging.AddEventSourceLogger();
-        //});
     }
 }
