79b9045737e808f539edf09cc12fced2fb6f8d0b
Josh Harmon <photigragraphy@gmail.com>
2019-12-23 10:22:32 -0800

    AboutMe: Add About me component, updating react routing, and default content

--- JoshHarmon.Site/ClientApp/src/App.js
+++ JoshHarmon.Site/ClientApp/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router";
 import { Layout } from "./components/Layout";
-import { Splash } from "./components/Splash/Splash";
-import { Projects } from "./components/Projects/Projects";
+import { AboutMe } from "./components/AboutMe/AboutMe";
 import { Blog } from "./components/Blog/Blog";
 import { BlogArticle } from "./components/Blog/BlogArticle";
+import { Projects } from "./components/Projects/Projects";
+import { Splash } from "./components/Splash/Splash";
 import { NotFound } from "./NotFound";
 
 export default class App extends Component {
@@ -32,10 +33,11 @@ export default class App extends Component {
             <Layout>
                 <Switch>
                     <Route exact path='/' component={Splash} />
-                    <Route exact path='/projects' component={Projects} />
+                    <Route exact path='/me' component={AboutMe} />
                     <Route exact path='/blog' component={Blog} />
                     <Route exact path='/blog/:id' component={BlogArticle} /> 
                     <Route exact path='/blog/:year/:month/:day/:fileKey' component={BlogArticle} /> 
+                    <Route exact path='/projects' component={Projects} />
                     <Route render={() => <NotFound route={exactRoute} />} />
                 </Switch>
             </Layout>
---
+++ JoshHarmon.Site/ClientApp/src/components/AboutMe/AboutMe.js
@@ -0,0 +1,18 @@
+﻿import React, { Component } from 'react';
+
+
+export class AboutMe extends Component {
+    static displayName = AboutMe.name;
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div class="page">
+            <h1>About Me</h1>
+            </div>
+        );
+    }
+}
