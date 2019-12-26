2591eefdb69fcc496dd49d40ea1edb2d4b96bb8f
Josh Harmon <photigragraphy@gmail.com>
2019-12-26 10:45:10 -0800

    AboutMe: Frame out structure for page with basic markup and css

--- JoshHarmon.Site/ClientApp/src/components/AboutMe/AboutMe.js
+++ JoshHarmon.Site/ClientApp/src/components/AboutMe/AboutMe.js
@@ -11,7 +11,14 @@ export class AboutMe extends Component {
     render() {
         return (
             <div class="page">
-            <h1>About Me</h1>
+                <div class="aboutMe">
+                    <img src="/assets/JoshOutOfFocus.jpg" />
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sodales at elit a ullamcorper. Suspendisse mattis sapien nec eros ornare euismod at quis massa. Aenean sit amet vulputate dolor. Praesent placerat hendrerit fringilla. Pellentesque aliquet lobortis porta. Fusce euismod rutrum orci tempor blandit. Vivamus gravida lobortis justo, non suscipit nulla gravida sit amet. Nunc sodales risus non consequat congue. Nulla nulla ipsum, mattis eu mi vitae, semper vestibulum velit. Aenean condimentum purus sed neque cursus semper. Maecenas blandit in tortor efficitur mattis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris ullamcorper id urna sed tempus.
+                    </p>
+                    <div class="aboutMeMain">
+                    </div>
+                </div>
             </div>
         );
     }
