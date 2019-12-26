d892e50e4da97f5b2b1fac389767166be3d35dec
Josh Harmon <photigragraphy@gmail.com>
2019-12-26 11:32:53 -0800

    AboutMe: Add `react-tabs` tab component with basic styling.
    Going to start having each components css referenced relatively from the component (and thus live in the same directory as the js) rather than having all of it referenced globally on index.html

--- JoshHarmon.Site/ClientApp/src/components/AboutMe/AboutMe.js
+++ JoshHarmon.Site/ClientApp/src/components/AboutMe/AboutMe.js
@@ -1,4 +1,6 @@
 ﻿import React, { Component } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import './about-me.css';
 
 
 export class AboutMe extends Component {
@@ -6,6 +8,37 @@ export class AboutMe extends Component {
 
     constructor(props) {
         super(props);
+    }
+
+    static buildTabs() {
+        return (
+            <div>
+                <Tabs
+                    className="tabs"
+                    selectedTabClassName="tabSelected"
+                    selectedTabPanelClassName="panelSelected"
+                >
+                    <TabList className="tabList">
+                        <Tab className="tab">Professional</Tab>
+                        <Tab className="tab">Personal</Tab>
+                    </TabList>
+                    <TabPanel className="tabPanel">
+                        {/* Professional */}
+                        <h3>Professional</h3>
+                        <p>
+                            Lorem dfsgsd dfg d dfg dfg dipsum dolor sit amet, consectetur adipiscing elit. In sodales at elit a ullamcorper. Suspendisse mattis sapien nec eros ornare euismod at quis massa. Aenean sit amet vulputate dolor. Praesent placerat hendrerit fringilla. Pellentesque aliquet lobortis porta. Fusce euismod rutrum orci tempor blandit. Vivamus gravida lobortis justo, non suscipit nulla gravida sit amet. Nunc sodales risus non consequat congue. Nulla nulla ipsum, mattis eu mi vitae, semper vestibulum velit. Aenean condimentum purus sed neque cursus semper. Maecenas blandit in tortor efficitur mattis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris ullamcorper id urna sed tempus.
+                        </p>
+                    </TabPanel>
+                    <TabPanel className="tabPanel">
+                        {/* Personal */}
+                        <h3>Personal</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sodales at elit a ullamcorper. Suspendisse mattis sapien nec eros ornare euismod at quis massa. Aenean sit amet vulputate dolor. Praesent placerat hendrerit fringilla. Pellentesque aliquet lobortis porta. Fusce euismod rutrum orci tempor blandit. Vivamus gravida lobortis justo, non suscipit nulla gravida sit amet. Nunc sodales risus non consequat congue. Nulla nulla ipsum, mattis eu mi vitae, semper vestibulum velit. Aenean condimentum purus sed neque cursus semper. Maecenas blandit in tortor efficitur mattis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris ullamcorper id urna sed tempus.
+                        </p>
+                    </TabPanel>
+                </Tabs>
+            </div>
+        );
     }
 
     render() {
@@ -16,8 +49,7 @@ export class AboutMe extends Component {
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sodales at elit a ullamcorper. Suspendisse mattis sapien nec eros ornare euismod at quis massa. Aenean sit amet vulputate dolor. Praesent placerat hendrerit fringilla. Pellentesque aliquet lobortis porta. Fusce euismod rutrum orci tempor blandit. Vivamus gravida lobortis justo, non suscipit nulla gravida sit amet. Nunc sodales risus non consequat congue. Nulla nulla ipsum, mattis eu mi vitae, semper vestibulum velit. Aenean condimentum purus sed neque cursus semper. Maecenas blandit in tortor efficitur mattis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris ullamcorper id urna sed tempus.
                     </p>
-                    <div class="aboutMeMain">
-                    </div>
+                    {AboutMe.buildTabs()}
                 </div>
             </div>
         );
